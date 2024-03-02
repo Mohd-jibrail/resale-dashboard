bf2bbedf5b3162bfaa2afc4f6ed38499dde79016
MohdJIbrail <mohdjibrail34@gmail.com>
2024-03-03 00:50:16 +0530

    Dashboarding in the midway

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter,Lusitana, Courgette } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight:['400','700'],
+    subsets: ['latin'] 
+});
+export const courgette = Courgette({
+    weight:['400','400'],
+    subsets:['latin']
+});
